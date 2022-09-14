97c0a6bec2ca678a338dffcc2522a858b1ec7813
Marko <Piirmarko92@gmail.com>
2022-09-14 20:54:06 +0300

    Add datatypes examples

--- src/Datatype.java
+++ src/Datatype.java
@@ -5,6 +5,7 @@
  */
 
 public class Datatype {
+    // Primitive Data types
     //Numbers
     int a = 1;
     byte byteNumber = 120;
@@ -30,5 +31,59 @@ public class Datatype {
     char animal; //''
 
     boolean isClassTomorrow; // false
+
+
+    // Non-primitive data types
+    String word = "I'm a developer";
+
+    Character random = 'u';
+
+    Integer number = 5;
+
+    Float numberSecond = 2.45F;
+
+    Integer randomNumber;   // null
+
+    // Multi-Declaration
+    int numberOne, numberTwo, numberThree;
+
+    String fruit = "apple", vegetable = "pumpkin", cities;
+
+
+    //Scope of class variables
+    private static String CAR = "Toyota";
+
+    public static String FOOD = "Cake";
+
+
+    public static void main(String[] args) {
+        int k = 0;
+        Integer y = null;
+
+
+        System.out.println(k);
+        System.out.println(y);
+
+
+
+        String _color = "black";
+
+        int $hexaNumber = 0x2A;
+
+        float exam12_mark =100.10f;
+
+        String className = "sda";
+
+        int a = 100;
+        int b = 0;
+        int c = a + b;
+
+        System.out.println(CAR);
+        System.out.println(FOOD);
+
+    }
+
+
+
 
 }
