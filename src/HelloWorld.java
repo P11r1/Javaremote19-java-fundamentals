78861d76b57ad14429070f541556d6f0b7b9d7bd
Marko <Piirmarko92@gmail.com>
2022-09-14 19:22:46 +0300

    HelloWorld done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
+/**
+ * The first hello world program
+ *
+ * @author Marko
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
 }
