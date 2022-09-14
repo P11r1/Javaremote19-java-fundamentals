e3123d7b511351917571513a3104b4f3d09404a5
Marko <Piirmarko92@gmail.com>
2022-09-14 20:05:33 +0300

    HelloWorld done

---
+++ src/Datatype.java
@@ -0,0 +1,34 @@
+/**
+ * Examples of primitive data types
+ *
+ * @author Marko
+ */
+
+public class Datatype {
+    //Numbers
+    int a = 1;
+    byte byteNumber = 120;
+    long longNumber = 56213565878L;
+    short shortNumber = -32000;
+    float decimalNumber = 2.45f;
+    double doubleDecimalNumber = 3.456d;
+
+    //character
+    char alpha = 'F';
+
+    //True/False
+    boolean hadLunch = true; //had lunch?
+    boolean isHoliday = false;
+    /* Multi-line
+    comment
+     */
+
+    int i; // 0
+
+    float x; // 0.00f
+
+    char animal; //''
+
+    boolean isClassTomorrow; // false
+
+}
