a3b306a486d093237439522dcafeb75fc238b415
Marko <Piirmarko92@gmail.com>
2022-09-20 02:38:27 +0300

    Homework, Java program that swaps variables

---
+++ src/Homework2.java
@@ -0,0 +1,32 @@
+/**
+ * Homework, Java program that swaps variables
+ *
+ * @author Marko
+ *
+ */
+
+public class Homework2 {
+    public static void main(String[] args) {
+       int c = 500;
+       int x = 200;
+
+        System.out.println("Before Swap");
+        System.out.println("c = " + x);
+        System.out.println("x = " + c);
+
+        int temp = c;
+        c = x;
+        x = temp;
+
+        System.out.println("After Swap");
+        System.out.println("c = " + x);
+        System.out.println("x = " + c);
+
+
+
+
+
+
+
+    }
+}
