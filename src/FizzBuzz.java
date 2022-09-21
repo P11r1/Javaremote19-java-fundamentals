f7ebc40cda90233d451453ae09a6596d4278f63d
Marko <Piirmarko92@gmail.com>
2022-09-21 16:19:53 +0300

    Added comments

---
+++ src/FizzBuzz.java
@@ -0,0 +1,16 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int j = 15;
+
+        if(j % 3 == 0 && j % 5 == 0){
+            System.out.println("FIZZBUZZ");
+        } else if(j % 3 == 0) {
+            System.out.println("FIZZ");
+        } else if(j % 5 == 0) {
+            System.out.println("BUZZ");
+        }
+
+        //solution 2
+        //Homework 3: one line for FIZZBUZZ
+    }
+}
