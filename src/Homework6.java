ef64ae23ad593f240e2625bb8b415b440144b05b
Marko <Piirmarko92@gmail.com>
2022-09-27 02:14:45 +0300

    Homework6 partly done

--- src/Homework6.java
+++ src/Homework6.java
@@ -2,13 +2,12 @@ public class Homework6 {
     public static void main(String[] args) {
 
 
-        int[] array = {1, 7, 3, 10, 9};
-        int result = array[0];
+        int[] a = {1, 7, 3, 10, 9};
+        int result = a[0];
 
-        for(int i = 0; i < array.length; i++) {
-            if(i > result) {
-                result = i;
-                //i--;
+        for(int x : a) {
+            if(x > result) {
+                result = x - 1;
             }
         }
         System.out.println(result);
