7c589f1b770c413072c55055e54efc8978de94c4
Marko <Piirmarko92@gmail.com>
2022-09-28 02:58:09 +0300

    Added some comments to StringExample

--- src/examples/StringExample.java
+++ src/examples/StringExample.java
@@ -14,13 +14,13 @@ public class StringExample {
 
         System.out.println(fullName);
 
-        firstName.concat(lastName); // option 2
+        firstName.concat(lastName); // option 2, concat means joining two Strings together
 
         String city = "Parnu";
 
-        String ageString = "My ages is : " + 3 + ". I ive in " + city + ". My name is: " + fullName;
+        String ageString = "My ages is : " + 3 + ". I ive in " + city + ". My name is: " + fullName; // this whole line is a String
         System.out.println(ageString);
-        // d% integer %s string
+                                                               // d% integer %s string
         String ageStringFormatted = String.format("My age is : %d. I live in %s. My name is: %s", 3, city, fullName);
         System.out.println(ageStringFormatted);
 
@@ -49,14 +49,14 @@ public class StringExample {
 
         String s1 = "Sibul"; // Pool
         String s2 = "Sibul"; // Pool
-        String s3 = new String("Sibul"); // Heap (1)//589498465
+        String s3 = new String("Sibul"); // Heap (1)//589498465 random memory value
         String s4 = new String("Sibul"); // Heap (1)//564864651
 
         System.out.println(s1 == s3); // False because stored in different memory
         System.out.println(s1 == s2);
-        System.out.println(s3 == s4); // (1)false because the memory values are different
+        System.out.println(s3 == s4); // (1)false because the memory values where string value is stored are different
 
-        System.out.println(s1.equals(s3)); // True because .equals compares also the memory
+        System.out.println(s1.equals(s3)); // True because .equals compares only values
 
 
     }
@@ -68,11 +68,12 @@ public class StringExample {
 
 
     /*  the power of 8
+                          62
         (6 * 8 pow 1)    +      (2 * 8 pow 0) = 48 + 2 = 50
 
                 3 * 8 pow 1 + 6 * 8 pow 0
 
-
+                           2874
  (2 * 8 pow 3) + (8 * 8 pow 2) + (7 * 8 pow 1) + (4 * 8 pow 0)
         (2 * 512) + (8 * 64) + (7 * 8) + (4 * 1)
                  1024 + 512 + 56 + 4
