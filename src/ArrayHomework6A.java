914a32c344516b7dacb739a381cec7b86f817e7f
Marko <Piirmarko92@gmail.com>
2022-09-28 00:12:24 +0300

    Homework6 fixed

--- src/ArrayHomework6A.java
+++ src/ArrayHomework6A.java
@@ -4,15 +4,13 @@
  * @author Marko
  */
 public class ArrayHomework6A {
+    public static void main(String[] args) {
+        int[] array = {1, 7, 3, 10, 9};
 
-    public ArrayHomework6A() {
-         int[] array = {1, 7, 3, 10, 9};
+        ArrayHomework6B arrayHomework6b = new ArrayHomework6B();
+        int result = arrayHomework6b.findSecondLargestNumber(array);
 
-
-
-
-
-
+        System.out.println("Second largest element in array: " + result);
     }
 }
 
--- src/ArrayHomework6B.java
+++ src/ArrayHomework6B.java
@@ -2,11 +2,9 @@
  * To find the second largest element in array
  *
  * @author Marko
- *
  */
 public class ArrayHomework6B {
     public int findSecondLargestNumber(int[] inputArray) {
-        int[] array = {1, 7, 3, 10, 9};
         int result = inputArray[0];
 
         for (int x : inputArray) {
