4a68e8bea6a0e1a34672bf5506d3a7538042fa3e
Marko <Piirmarko92@gmail.com>
2022-10-05 00:55:36 +0300

    Homework done

--- src/homework/IntegerDisplay.java
+++ src/homework/IntegerDisplay.java
@@ -1,5 +1,7 @@
 package homework;
 
+import java.util.Scanner;
+
 /**
  * To read a number from user and print the same number like this:
  *
@@ -8,4 +10,15 @@ package homework;
  * @author Marko
  */
 public class IntegerDisplay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number to print:");
+        int number = scanner.nextInt();
+
+        System.out.println("Numbers:");
+        System.out.println(number + ", " + number + number + ", " + number + number + number + ", " + number + number + number + number);
+        System.out.println();
+    }
+
 }
