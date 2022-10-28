c8b7f80270099c6475e1a3df80f282ea708c3029
Marko <Piirmarko92@gmail.com>
2022-10-29 00:31:23 +0300

    Petrol Station2 exercise done

--- src/homework/PetrolStation.java
+++ src/homework/PetrolStation.java
@@ -1,5 +1,6 @@
 package homework;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -17,14 +18,15 @@ public class PetrolStation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
         double petrol = 1;
         double price = 1.89;
 
 
-        System.out.println("PETROL STATION \n---------------");
+        System.out.println("PETROL STATION \n--------------");
         System.out.println("Refilling");
 
-        System.out.println("Do you want to continue refilling?");
+        System.out.println("Do you want to refill the tank?");
         boolean isContinue = scanner.nextBoolean();
 
         double totalAmount = 0;
@@ -41,21 +43,62 @@ public class PetrolStation {
 
             System.out.println("Do you want to continue?");
             isContinue = scanner.nextBoolean();
-
-
         }
 
-        System.out.println("Do you want to pay?");
-        isContinue = scanner.nextBoolean();
+        //Payment
+        if (totalAmount > 0) {
+            System.out.println("Do you wish to pay now? (True/False)");
+            System.out.println("TotalLitres: " + totalLitres + "L");
+            System.out.println("Total amount: " + totalAmount + " Eur");
 
-        while (isContinue) {
-            if (scanner.nextBoolean()) {
-                System.out.println("Amount to be paid: " + totalAmount);
+            if (isContinue()) {
+                boolean isMoneyPayed = false;
+
+                while (!isMoneyPayed) {
+                    double money = getMoney();
+                    System.out.println("Money paid: " + money + "Eur");
+                    String message = "Thanks for the payment! Visit us again!";
+
+                    if (money == totalAmount) { // User paid exactly as much as required
+                        System.out.println(message);
+                        isMoneyPayed = true;
+                    } else if (money > totalAmount) { // The user paid too much
+                        double balance = money - totalAmount;
+                        System.out.println("Balance return: " + balance + "Eur");
+                        System.out.println(message);
+                        isMoneyPayed = true;
+                    } else { // User paid too little
+                        System.out.println("The amount paid is lesser than the total amount! Please pay the correct amount!");
+                    }
+                }
             } else {
-                System.out.println("Invalid option. Please enter again!");
+                System.out.println("Invoice will be sent to you! Thank you!");
             }
         }
+    }
+    private static boolean isContinue() {
+        Scanner scanner = new Scanner(System.in);
+        String errorMessage = "Incorrect answer. Please enter again:";
+        boolean checker = false;
+        boolean answer = false;
 
+
+        do {
+            if (!scanner.hasNextBoolean()) {
+                System.out.println(errorMessage);
+                scanner.next();
+            } else {
+                answer = scanner.nextBoolean();
+                checker = true;
+            }
+
+        } while (!checker);
+
+        return answer;
+    }
+    //Random amount of money
+    private static int getMoney() {
+        return new Random().nextInt(2000 - 1) + 1;
     }
 }
 
